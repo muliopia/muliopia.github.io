0a637974a7f572c0689fdc77ffd2335c976af07d
muliopia <mulugetasahil31@gmail.com>
2024-10-19 10:44:30 -0300

    first boom

--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/chappiii/chappiii.github.io/master/src/assets/Mulugeta%20Resume.pdf";
+  "https://raw.githubusercontent.com/muliopia/muliopia.github.io/master/src/assets/Mulugeta%20Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
