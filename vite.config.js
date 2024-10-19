5841eeb9051f31905cf649b2e995656943722137
muliopia <mulugetasahil31@gmail.com>
2024-10-19 17:51:49 -0300

    fix the error

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Export the Vite configuration
 export default defineConfig({
-  // Build options
-  build: {
-    rollupOptions: {
-      output: {
-        // Custom chunking logic
-        manualChunks(id) {
-          // Check if the module is from node_modules
-          if (id.includes('node_modules')) {
-            // Create a chunk for each npm package
-            return id.toString().split('node_modules/')[1].split('/')[0];
-          }
-        },
-      },
-    },
-  },
-  // Base URL for the project
-  base: "/",
-  // Plugins to be used
   plugins: [react()],
 });
