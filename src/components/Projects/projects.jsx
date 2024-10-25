f52f17f3db48ea0392be495a8954306b0bd2d4f3
muliopia <mulugetasahil31@gmail.com>
2024-10-25 09:12:16 -0300

    unchanged

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -53,7 +53,7 @@ function Projects() {
                   v25 QR Code Scanner
                 </Card.Title>
                 <Card.Text className="mb-3 mx-2">
-            using react Native
+                  Flutter mobile app to scan QR codes version 25
                 </Card.Text>
                 <Button
                   href="https://github.com/muliopia"
